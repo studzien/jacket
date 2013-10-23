511b6261915ce97c1f1b45c758e0ea6d0824548a
Rafał Studnicki <rafal@opencubeware.org>
2013-10-23 20:10:28 +0200

    Rename in javascript

--- priv/jacket.js
+++ priv/jacket.js
@@ -17,7 +17,8 @@
 		this.onmessage = function() {};
 		
 		this.serialize = function(data) {
-            var term = Bert.encode(data);
+		    var bertData = Bert.tuple(data.type, data.timestamp, data.message);
+            var term = Bert.encode(bertData);
             var len = term.length;
             var byteArray = new Uint8Array(len);
             for (var i=0; i<len; ++i) {
@@ -29,13 +30,23 @@
 		this.deserialize = function(data) {
             var byteArray = Base64Binary.decode(data);
             var byteString = Bert.bytes_to_string(byteArray);
-            return Bert.decode(byteString); 
+            var term = Bert.decode(byteString); 
+            if(term.type == 'Tuple' && term.length == 3) {
+                return {type: term[0].value,
+                        timestamp: term[1],
+                        message: term[2]};
+            }
+            else {
+                return undefined;
+            }
 		};
 
         this.call = function(message, timeout) {
             var timeout = timeout || 5000;
             var timestamp = ++ts;
-            var term = Bert.tuple(Bert.atom("call"), timestamp, message);
+            var term = {type: 'call',
+                        timestamp: timestamp,
+                        message: message}
             var deferred = Q.defer();
             console.log("call: " + term);
             var promise = deferred.promise
@@ -55,7 +66,9 @@
 
         this.cast = function(message) {
             var timestamp = ++ts;
-            var term = Bert.tuple(Bert.atom("cast"), timestamp, message);
+            var term = {type: 'cast',
+                        timestamp: timestamp,
+                        message: message}
             console.log("cast: " + term);
             send(term);
         };
@@ -70,37 +83,33 @@
 
         bullet.onopen = function() {
             self.onopen();    
-            console.log('bullet-bert connection opened!');
+            console.log('jacket connection opened!');
         };
 
         bullet.onclose = function() {
             self.onclose();
-            console.log('bullet-bert connection closed!');
+            console.log('jacket connection closed!');
         };
 
         bullet.onmessage = function(message) {
             var data = message.data;
-            if(data == "pong") {
+            if(data == 'pong') {
                 return;
             }
             var term = self.deserialize(data);
-            if(term.type == "Tuple" && term.length == 3
-               && term[0].type == "Atom") {
-                if(term[0].value == "reply") {
-                    console.log("reply: " + term);
-                    var timestamp = term[1];
-                    var deferred = deferreds[timestamp];
-                    deferred.resolve(term[2]);
-                }
-                else if(term[0].value == "info") {
-                    self.onmessage(term[2]);
-                    ts = Math.max(term[1], ts+1);
-                }
+            if(term && term.type == 'reply') {
+                console.log('reply: ' + term);
+                var deferred = deferreds[term.timestamp];
+                deferred.resolve(term.message);
+            }
+            else if(term && term.type == 'info') {
+                self.onmessage(term.message);
+                ts = Math.max(term.timestamp, ts+1);
             }
         };
 
         bullet.onheartbeat = function() {
-            bullet.send("ping");
+            bullet.send('ping');
         };
     };
 
